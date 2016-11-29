817e1b5c0bb32370b1a5166d33666add7d70ff5d
Einar Kjartansson <eik.klaki.net>
2016-11-29 14:09:05 +0000

    readCtloc working

--- phases.c
+++ phases.c
@@ -4,11 +4,55 @@
 #include <math.h>
 #include <time.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <fenv.h>
 #include "ray.h"
-VelModel mS, mP ;
 
-void readPhases( char *filename)
+VelModel mS, mP ;
+off_t testFileSize(char *fileName)
+{
+	int fd;
+	off_t size ;
+	fd = open(fileName,O_RDONLY) ;
+	size = lseek(fd,0,SEEK_END) ;
+	(void) close(fd) ;
+	return size ;
+}
+int readCtloc(char *fileName, Solution *solutions )
+{
+#define CTLINELENGTH 133 
+	int size ,nSol, i,ii, nf ;
+	char *line[300] ;
+	FILE *ffd ;
+	Solution *sp ;
+/*	char *fields[200] ; */
+	size = testFileSize(fileName) ;
+	nSol = size/CTLINELENGTH ;
+	solutions = (Solution*)  malloc(sizeof(Solution) * (nSol+5) ) ;
+	sp = solutions ;
+	ffd = fopen(fileName,"r") ;
+	while( nf = readData(ffd,line) ) {
+		sp->index = strtoll(line[0],NULL,10) ;
+		sp->lat = atof(line[3]) ;
+		sp->lon = atof(line[4]) ;
+		sp->depth = atof(line[5]) ;
+		sp++ ;
+	}
+	return sp-solutions ;
+/*
+	ii = 0 ;
+	for( i = 0 ; i < nSol; i++) {
+		sp = solutions + i ;
+		printf("%3d %ld %19ld %9.5f %9.5f %8.3f\n",
+			i,sp->index,sp->index-solutions[ii].index,sp->lat,sp->lon,sp->depth) ;
+		ii = i ;
+	}
+*/
+}
+void testPhases( char *filename)
 {
 	FILE *fd ;
 	int nf ;
@@ -45,8 +89,10 @@ void readPhases( char *filename)
 }
 
 int main(int ac, char **av) {
+	Solution  *sol ;
 /*	feenableexcept(FE_INVALID) ; */
 	shLogLevel = 2 ;
-	readPhases("../geysir/phase.dat") ;
+	readCtloc("../geysir/ctloc2",sol) ;
+	testPhases("../geysir/phase.dat") ; 
 	return 0 ;
 }
